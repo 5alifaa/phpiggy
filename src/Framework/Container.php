5f5fc859a452c3221592dd110e450a7dde7f9306
Mohamed Khalifa <67746067+5alifaa@users.noreply.github.com>
2026-01-19 14:22:07 +0200

    retrieve the constructor param

--- src/Framework/Container.php
+++ src/Framework/Container.php
@@ -28,6 +28,12 @@ class Container
             return new $className;
         }
 
-        dd($constructor);
+        $params = $constructor->getParameters();
+
+        if (count($params) === 0) {
+            return new $className;
+        }
+        
+        dd($params);
     }
 }
